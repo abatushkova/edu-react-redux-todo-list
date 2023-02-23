2e31e5f83a1f16819a5d2f87d0ac8debc137bb68
Alena Batushkova <alena.batushkova@gmail.com>
2023-02-23 20:58:50 +0300

    add filter styles

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Filter.scss';
 
 interface IProps {
   filter: string;
--- src/components/FilterList/FilterList.tsx
+++ src/components/FilterList/FilterList.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Filter } from '../Filter/Filter';
+import './FilterList.scss';
 
 export const FilterList = () => {
   return (
     <div className="filter-list">
-      <span className="filter-list__title">Show</span>
+      <span className="filter-list__title">Show:</span>
       <Filter filter="SHOW_ALL">All</Filter>
       <Filter filter="SHOW_ACTIVE">Active</Filter>
       <Filter filter="SHOW_COMPLETED">Completed</Filter>
